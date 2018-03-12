482abb261f6c71baa95339b636dbaad62ef02d24
alik <alik@DESKTOP-0B7P8PB>
2018-03-12 19:57:03 +0100

    First commit

---
+++ src/zoo/AnimalInterface.java
@@ -0,0 +1,13 @@
+package zoo;
+/**
+ * Very basic animal interface
+ * @author alik
+ * @version 1.0
+ */
+public interface AnimalInterface {
+	public double getWeight();
+	public void setWeight(int weight);
+	public double foodConsumption();
+	public String doSound();
+	public String whoAmI();
+}
