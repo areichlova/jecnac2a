0d785d51778a777c8375958f16d068caa2855eab
vana3 <vana3@PC538.SPSEJECNANET.CZ>
2018-03-13 11:10:26 +0100

    Kojot

--- src/zoo/Kojot.java
+++ src/zoo/Kojot.java
@@ -1,5 +1,42 @@
 package zoo;
 
-public class Kojot {
+public class Kojot implements AnimalInterface {
+private int weight;
+private String name;
+	
+	
+	public Kojot(int weight, String name) {
+	this.weight =  weight;
+	this.name = name;
+}
+
+	@Override
+	public double getWeight() {
+		// TODO Auto-generated method stub
+		return weight;
+	}
+
+	@Override
+	public void setWeight(int weight) {
+		
+	}
+
+	@Override
+	public double foodConsumption() {
+		// TODO Auto-generated method stub
+		return weight/4;
+	}
+
+	@Override
+	public String doSound() {
+		// TODO Auto-generated method stub
+		return "[Zvuk kojota]";
+	}
+
+	@Override
+	public String whoAmI() {
+		// TODO Auto-generated method stub
+		return name;
+	}
 
 }
