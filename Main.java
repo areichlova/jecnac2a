d69e32109183ee3af642b05e82fc94de4ba18c82
Vít Vágner <Peiryn@gmail.com>
2018-03-13 10:48:29 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package zoo;
+
+import java.util.ArrayList;
+
+/**
+ * Entry class
+ * @author alik
+ * @version 1.0
+ */
+public class Main {
+	
+	//Zde jsem nìco napsal.
+
+	public static void main(String[] args) {
+		ArrayList<AnimalInterface> animals = new ArrayList<>();
+		
+		//Zde vytvorte objekt sve tridy a vlozte ho do ArrayListu
+		
+		for(AnimalInterface anim : animals) {
+			System.out.println(anim.whoAmI()+": "+anim.doSound());
+		}
+	}
+
+}
