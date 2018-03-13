56b2d5b5c3ccf0415c391ed070137f5e9c754cf3
dumalas <dumalas@PC543.SPSEJECNANET.CZ>
2018-03-13 11:06:48 +0100

    add Bizon

---
+++ src/zoo/Bizon.java
@@ -0,0 +1,43 @@
+package zoo;
+
+public class Bizon implements AnimalInterface{
+	private double weight;
+	private int dotNumber;
+
+	public Bizon(double weight, int dotNumber) {
+		super();
+		this.weight = weight;
+		this.dotNumber = dotNumber;
+	}
+
+	@Override
+	public double getWeight() {
+		// TODO Auto-generated method stub
+		return weight;
+	}
+
+	@Override
+	public void setWeight(int weight) {
+		// TODO Auto-generated method stub
+		this.weight = weight;
+	}
+
+	@Override
+	public double foodConsumption() {
+		// TODO Auto-generated method stub
+		return weight;
+	}
+
+	@Override
+	public String doSound() {
+		// TODO Auto-generated method stub
+		return "Buuuu, Buuuu!";
+	}
+
+	@Override
+	public String whoAmI() {
+		// TODO Auto-generated method stub
+		return "Bizon: ";
+	}
+
+}
