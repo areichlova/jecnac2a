678e9c2f62bd41fad4470cda70d1b1327f61fcdd
simek2 <simek2@PC534.SPSEJECNANET.CZ>
2018-03-13 11:05:44 +0100

    add Lev

---
+++ src/zoo/Lev.java
@@ -0,0 +1,35 @@
+package zoo;
+
+public class Lev implements AnimalInterface {
+
+	@Override
+	public double getWeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setWeight(int weight) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double foodConsumption() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String doSound() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String whoAmI() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
