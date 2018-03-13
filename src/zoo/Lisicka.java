b6d385476798ebcfefe592e54c26070b0e4ce673
hrinchenko <hrinchenko@PC542.SPSEJECNANET.CZ>
2018-03-13 11:14:25 +0100

    add lisicka

--- src/zoo/Lisicka.java
+++ src/zoo/Lisicka.java
@@ -19,7 +19,7 @@ public class Lisicka implements AnimalInterface{
 	@Override
 	public void setWeight(double weight) {
 		// TODO Auto-generated method stub
-		this.weight = weight;
+		this.weight = weight; 
 	}
 
 	@Override
