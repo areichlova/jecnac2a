d84338ec12ef1bf279416e03f519824f74d3fa17
hanus2 <hanus2@PC525.SPSEJECNANET.CZ>
2018-03-13 11:22:40 +0100

    khgiug

--- src/zoo/Main.java
+++ src/zoo/Main.java
@@ -8,27 +8,27 @@ import java.util.ArrayList;
  * @version 1.1
  */
 public class Main {
-<<<<<<< HEAD
+
 	
 	//Zde jsem n�co napsal.
 	
-=======
+
 	 
->>>>>>> branch 'master' of https://github.com/areichlova/jecnac2a.git
+
 
 	public static void main(String[] args) {
 		ArrayList<AnimalInterface> animals = new ArrayList<>();
 		
-<<<<<<< HEAD
+
 		//Zde vytvorte objekt sve tridy a vlozte ho do ArrayListu
-=======
+
 		System.out.println("Ščančar mi dluží prachy, berušky.");
 		
 		animals.add(new Lenochod());
 			
 		animals.add(new Yeti());
 		animals.add(new Hroch(269,2.5));
->>>>>>> branch 'master' of https://github.com/areichlova/jecnac2a.git
+
 		
 		for(AnimalInterface anim : animals) {
 			System.out.println(anim.whoAmI()+": "+anim.doSound());
