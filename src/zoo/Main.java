a13e6cc8f319c3eea8fcadf52eef2da9ff0cc721
sevcik3 <sevcik3@PC547.SPSEJECNANET.CZ>
2018-03-13 10:53:45 +0100

    Test

--- src/zoo/Main.java
+++ src/zoo/Main.java
@@ -9,10 +9,12 @@ import java.util.ArrayList;
  */
 public class Main {
 	
-	//Zde jsem n�co napsal.
+	//Zde jsem n�co napsal.
 
 	public static void main(String[] args) {
 		ArrayList<AnimalInterface> animals = new ArrayList<>();
+		
+		System.out.println("Ščančar mi dluží prachy, berušky.");
 		
 		//Zde vytvorte objekt sve tridy a vlozte ho do ArrayListu
 		
