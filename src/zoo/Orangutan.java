f1fc5fa964053fbe327a1fbe54278da75f7746e9
kolman <kolman@PC517.SPSEJECNANET.CZ>
2018-03-13 11:11:15 +0100

    add Orangutan

--- src/zoo/Orangutan.java
+++ src/zoo/Orangutan.java
@@ -4,34 +4,26 @@ public class Orangutan implements AnimalInterface {
 
 	private double weight;
 	
-	
-	
-	
-	@Override
 	public double getWeight() {
 		
 		return 0;
 	}
 
-	@Override
 	public void setWeight(int weight) {
 		this.weight = weight;
 		
 	}
 
-	@Override
 	public double foodConsumption() {
 		
 		return weight*2;
 	}
 
-	@Override
 	public String doSound() {
 		
 		return "Random zvuk";
 	}
 
-	@Override
 	public String whoAmI() {
 		
 		return "Kryšpín";
