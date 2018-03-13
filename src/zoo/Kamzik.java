ca74772c22e5dfc1c1df48674729b4f28899c030
kozel <kozel@PC539.SPSEJECNANET.CZ>
2018-03-13 11:08:21 +0100

    Třída Kamzik

--- src/zoo/Kamzik.java
+++ src/zoo/Kamzik.java
@@ -1,5 +1,36 @@
 package zoo;
 
-public class Kamzik {
+public class Kamzik implements AnimalInterface {
+private double weight;
+private double delkaParohu;
+	@Override
+	public double getWeight() {
+		// TODO Auto-generated method stub
+		return weight;
+	}
+
+	@Override
+	public void setWeight(int weight) {
+		// TODO Auto-generated method stub
+		this.weight = weight;
+	}
+
+	@Override
+	public double foodConsumption() {
+		// TODO Auto-generated method stub
+		return weight*2;
+	}
+
+	@Override
+	public String doSound() {
+		// TODO Auto-generated method stub
+		return "Nìco co dìlá kamzík";
+	}
+
+	@Override
+	public String whoAmI() {
+		// TODO Auto-generated method stub
+		return "Kamzík";
+	}
 
 }
