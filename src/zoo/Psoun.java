71382a4c43b58ded8db5553ed42cd24d15424ac1
glas <glas@PC546.SPSEJECNANET.CZ>
2018-03-13 10:58:43 +0100

    Added Psoun

---
+++ src/zoo/Psoun.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package zoo;
+
+/**
+ * @author glas
+ *
+ */
+public class Psoun implements AnimalInterface{
+
+	private double weight;
+	private String sound;
+	private String identity;
+	
+	@Override
+	public double getWeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setWeight(int weight) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double foodConsumption() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String doSound() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String whoAmI() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
