ff42e505b3a94c7c3d24d9e9d9fd694a5f98788b
BuildTools <unconfigured@null.spigotmc.org>
2018-03-13 11:34:16 +0100

    pridan Fenek do animals

--- src/zoo/Main.java
+++ src/zoo/Main.java
@@ -26,15 +26,14 @@ public class Main {
 		System.out.println("Ščančar mi dluží prachy, berušky.");
 		
 		animals.add(new Lenochod());
-<<<<<<< HEAD
+
 			
+		animals.add(new Yeti());	
 		animals.add(new Yeti());
-=======
-		
-		animals.add(new Yeti());
->>>>>>> branch 'master' of https://github.com/areichlova/jecnac2a.git
 		animals.add(new Hroch(269,2.5));
 		animals.add(new Kun());
+		
+		animals.add(new Fenek(1.2));
 
 		
 		animals.add(new Fenek(1.2));
