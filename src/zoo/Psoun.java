30682d47025db0ef0c025f962499d4cf1de3af3b
glas <glas@PC546.SPSEJECNANET.CZ>
2018-03-13 11:11:52 +0100

    Add Psoun

--- src/zoo/Psoun.java
+++ src/zoo/Psoun.java
@@ -20,7 +20,7 @@ public class Psoun implements AnimalInterface{
 	}
 
 	@Override
-	public void setWeight(int weight) {
+	public void setWeight(double weight) {
 		// TODO Auto-generated method stub
 		this.weight = weight;
 	}
